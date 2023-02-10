a9d2c2ed28e16d9c8f21d6a63324e43051012c4e
Ayouuuu <arobcher233@163.com>
2023-02-11 01:26:38 +0800

    will-fix: Cookie not work that must be use 'response.set_cookie()' todo...

--- main.py
+++ main.py
@@ -9,25 +9,24 @@ app = FastAPI()
 
 
 @app.get("/info", description="获取用户信息")
-async def info(cookie: Optional[str] = Header(None)):
-    print(cookie)
-    return get_user_info(cookie)
+async def info(Cookie: Optional[str] = Header(None)):
+    return get_user_info(Cookie)
 
 
 "获取楼栋号列表"
 
 
 @app.get("/selfhelp/areas", description="获取楼栋号列表")
-async def selfhelp_areas(cookie: Optional[str] = Header(None)):
-    return get_areas(cookie)
+async def selfhelp_areas(Cookie: Optional[str] = Header(None)):
+    return get_areas(Cookie)
 
 
 "获取楼层号列表"
 
 
 @app.get("/selfhelp/houses", description="获取楼层号列表")
-async def selfhelp_houses(area_id: str = Query(..., description="楼栋号id"), cookie: Optional[str] = Header(None)):
-    return get_houses(area_id, cookie)
+async def selfhelp_houses(area_id: str = Query(..., description="楼栋号id"), Cookie: Optional[str] = Header(None)):
+    return get_houses(area_id, Cookie)
 
 
 "获取当前楼层所有房间余额"
@@ -36,5 +35,5 @@ async def selfhelp_houses(area_id: str = Query(..., description="楼栋号id"), 
 @app.get("/selfhelp/rooms", description="获取当前楼层所有房间余额")
 async def selfhelp_room(area_id: str = Query(..., description="楼栋号id"),
                         house_id: str = Query(..., description="楼层号id"),
-                        cookie: Optional[str] = Header(None)):
-    return get_rooms(area_id, house_id, cookie)
+                        Cookie: Optional[str] = Header(None)):
+    return get_rooms(area_id, house_id, Cookie)
