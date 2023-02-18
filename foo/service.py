73dfd0f5d26baba94c32d0bddfceacc1ff9ba212
Ayouuuu <arobcher233@163.com>
2023-02-18 19:07:37 +0800

    perf: 优化接口，采用 Session 统一管理 cookie,取消了传参

--- foo/service.py
+++ foo/service.py
@@ -1,5 +1,3 @@
-import json
-
 from pyquery import PyQuery as pq
 from foo.pojo.studeninfo import StudentInfo
 from foo.util import ocr_code
@@ -8,6 +6,7 @@ from foo.influxdb import *
 import requests
 import time
 import ujson
+import threading
 
 """
 验证码
@@ -25,6 +24,7 @@ url = [
 ]
 
 KEY = "ASP.NET_SessionId"
+session = requests.Session()
 
 """
 获取验证码
@@ -40,44 +40,38 @@ def authcode():
     with open('code.png', 'wb') as file:
         for data in response.iter_content(128):
             file.write(data)
-    return [ocr_code("code.png").upper(), response.cookies.get(KEY)]
+    cookie = response.cookies.get(KEY)
+    session.cookies.set(KEY, cookie)
+    return [ocr_code("code.png").upper(), cookie]
 
 
 "登陆"
 
 
-def auth_login(username: str, password: str, code: str, cookie: str):
+def auth_login(username: str, password: str, code: str):
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
         "Upgrade-Insecure-Requests": "1",
         "Referer": "http://10.14.0.124/zytk35portal/default.aspx",
-        "Cookie": cookie
     }
-    response = requests.post(url[4], headers=headers,
-                             data="__EVENTTARGET=UserLogin%24ImageButton1&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTc0MDQ4ODc3Nw9kFgICAQ9kFhICAw8WAh4HVmlzaWJsZWhkAgUPFgIfAGhkAgcPFgIfAGhkAgkPPCsACQEADxYEHghEYXRhS2V5cxYAHgtfIUl0ZW1Db3VudGZkZAIODzwrAAkBAA8WBB8BFgAfAmZkZAIQDzwrAAkBAA8WBB8BFgAfAmZkZAISDzwrAAkBAA8WBB8BFgAfAmZkZAIUDzwrAAkBAA8WBB8BFgAfAmZkZAIWDzwrAAkBAA8WBB8BFgAfAmZkZGT7t2PaAC71nhjomcWWlS2Kl%2FDh6Icnffmmf1QCBjcqOg%3D%3D&__VIEWSTATEGENERATOR=E655DDB2&__EVENTVALIDATION=%2FwEdAAmmkB23XPRc6QJjrAxJx7jUohjo8sIky4Xs%2BCUBsum%2BnL6pRh%2FvC3eYiguVzFy%2FtEYvT53BE9ULYNj8jfQiCQeC35ZbbeGbJddowj1pY7sNivrI0G85IvfKPX4CghIMZ1NJ4PbCb80KUDHFYYKXgFT9PjMyUg6NAZP4%2BvrIPkQUuFOdcKl43UA3HbIoQpEPelhEPm0OSqwYeIaEyD7zfAzjHEWZ1PjzcAqdQtFUyg1jBg%3D%3D&UserLogin%3AtxtUser=" + username + "&UserLogin%3AtxtPwd=" + password + "&UserLogin%3AddlPerson=%BF%A8%BB%A7&UserLogin%3AtxtSure=" + code)
-    info = get_user_info(cookie)
+    session.post(url[4], headers=headers, data="__EVENTTARGET=UserLogin%24ImageButton1&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTc0MDQ4ODc3Nw9kFgICAQ9kFhICAw8WAh4HVmlzaWJsZWhkAgUPFgIfAGhkAgcPFgIfAGhkAgkPPCsACQEADxYEHghEYXRhS2V5cxYAHgtfIUl0ZW1Db3VudGZkZAIODzwrAAkBAA8WBB8BFgAfAmZkZAIQDzwrAAkBAA8WBB8BFgAfAmZkZAISDzwrAAkBAA8WBB8BFgAfAmZkZAIUDzwrAAkBAA8WBB8BFgAfAmZkZAIWDzwrAAkBAA8WBB8BFgAfAmZkZGT7t2PaAC71nhjomcWWlS2Kl%2FDh6Icnffmmf1QCBjcqOg%3D%3D&__VIEWSTATEGENERATOR=E655DDB2&__EVENTVALIDATION=%2FwEdAAmmkB23XPRc6QJjrAxJx7jUohjo8sIky4Xs%2BCUBsum%2BnL6pRh%2FvC3eYiguVzFy%2FtEYvT53BE9ULYNj8jfQiCQeC35ZbbeGbJddowj1pY7sNivrI0G85IvfKPX4CghIMZ1NJ4PbCb80KUDHFYYKXgFT9PjMyUg6NAZP4%2BvrIPkQUuFOdcKl43UA3HbIoQpEPelhEPm0OSqwYeIaEyD7zfAzjHEWZ1PjzcAqdQtFUyg1jBg%3D%3D&UserLogin%3AtxtUser=" + username + "&UserLogin%3AtxtPwd=" + password + "&UserLogin%3AddlPerson=%BF%A8%BB%A7&UserLogin%3AtxtSure=" + code)
+    info = get_user_info()
     return info
 
 
 "获取用户信息"
 
 
-def get_user_info(cookie: str):
-    headers = {
-        'Cookie': cookie,
-    }
-    response = requests.get(url[1], headers=headers)
+def get_user_info():
+    response = session.get(url[1])
     doc = pq(response.text)
     studentInfo = StudentInfo(doc)
     studentInfo.createTime = (int(time.time()))
     return studentInfo.__dict__
 
 
-def get_areas(cookie: str):
-    headers = {
-        'Cookie': cookie
-    }
-    response = requests.get(url[2], headers=headers)
+def get_areas():
+    response = session.get(url[2])
     doc = pq(response.text)
     areas = doc.find("#lsArea").items("option")
     arr = []
@@ -89,16 +83,15 @@ def get_areas(cookie: str):
     return arr
 
 
-def get_houses(area_id: str, cookie: str):
+def get_houses(area_id: str):
     headers = {
-        'Cookie': cookie,
         'X-AjaxPro-Method': "GetHouseList",
         "Content-Type": "text/plain"
     }
     data = {
         "AreaId": area_id
     }
-    response = requests.post(url[3], headers=headers, data=ujson.dumps(data))
+    response = session.post(url[3], headers=headers, data=ujson.dumps(data))
     text = ujson.loads(response.text.replace(
         "new Ajax.Web.DataSet([new Ajax.Web.DataTable([[\"showvalue\",\"System.String\"],[\"showname\",\"System.String\"]],",
         "").replace(")]);/*", ""))
@@ -116,9 +109,8 @@ def get_houses(area_id: str, cookie: str):
     return d
 
 
-def get_rooms(area_id, house_id, cookie):
+def get_rooms(area_id, house_id):
     headers = {
-        'Cookie': cookie,
         'X-AjaxPro-Method': "GetRoomList",
         "Content-Type": "text/plain"
     }
@@ -126,7 +118,7 @@ def get_rooms(area_id, house_id, cookie):
         "areaId": area_id,
         "HouseId": house_id
     }
-    response = requests.post(url[3], headers=headers, data=ujson.dumps(data))
+    response = session.post(url[3], headers=headers, data=ujson.dumps(data))
     text = ujson.loads(response.text.replace(
         "new Ajax.Web.DataSet([new Ajax.Web.DataTable([[\"MeterID\",\"System.Int32\"],[\"Remnant\",\"System.Decimal\"],[\"showname\",\"System.String\"],[\"room_id\",\"System.String\"]],",
         "").replace(")]);/*", ""))
@@ -145,3 +137,16 @@ def get_rooms(area_id, house_id, cookie):
         "rooms": array
     }
     return data
+
+
+def write_room(area_id: str):
+    try:
+        get_areas()
+        houses = get_houses(area_id)
+        for house in houses["houses"]:
+            threading.Thread(target=write_point, args=(json.dumps(get_rooms(area_id, house['id'])),)).start()
+            time.sleep(1)
+        return {"code": 200, "message": "写入成功"}
+    except Exception as e:
+        return {"code": "500",
+                "message": "写入失败,请检查网络或者influxdb是否正常运行,以及电费网站是否可正常访问，如不能访问需要等待半小时或者更长！"}
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from fastapi import FastAPI, Header, Query
+from fastapi import FastAPI, Query
 
-from typing import Optional
 from starlette.responses import Response
 from foo.service import *
-
-import threading
-import time
 
 app = FastAPI()
 
@@ -16,7 +12,7 @@ app = FastAPI()
 async def login(response: Response, username: str, password: str):
     code = authcode()
     response.set_cookie(key=KEY, value=code[1])
-    info = auth_login(username, password, code[0], KEY + "=" + code[1])
+    info = auth_login(username, password, code[0])
     if info['name'] == '':
         return {"code": 403, "message": "无法获取登陆信息，请再次尝试或等待几分钟"}
     return {"userinfo": info, "cookie": code[1]}
@@ -26,24 +22,24 @@ async def login(response: Response, username: str, password: str):
 
 
 @app.get("/info", description="获取用户信息，初始化 cookie ")
-async def info(Cookie: Optional[str] = Header(None)):
-    return get_user_info(Cookie)
+async def info():
+    return get_user_info()
 
 
 "获取楼栋号列表"
 
 
 @app.get("/selfhelp/areas", description="获取楼栋号列表")
-async def selfhelp_areas(Cookie: Optional[str] = Header(None)):
-    return get_areas(Cookie)
+async def selfhelp_areas():
+    return get_areas()
 
 
 "获取楼层号列表"
 
 
 @app.get("/selfhelp/houses", description="获取楼层号列表")
-async def selfhelp_houses(area_id: str = Query(..., description="楼栋号id"), Cookie: Optional[str] = Header(None)):
-    return get_houses(area_id, Cookie)
+async def selfhelp_houses(area_id: str = Query(..., description="楼栋号id")):
+    return get_houses(area_id)
 
 
 "获取当前楼层所有房间余额"
@@ -51,14 +47,11 @@ async def selfhelp_houses(area_id: str = Query(..., description="楼栋号id"), 
 
 @app.get("/selfhelp/rooms", description="获取当前楼层所有房间余额")
 async def selfhelp_room(area_id: str = Query(..., description="楼栋号id"),
-                        house_id: str = Query(..., description="楼层号id"),
-                        Cookie: Optional[str] = Header(None)):
-    return get_rooms(area_id, house_id, Cookie)
+                        house_id: str = Query(..., description="楼层号id")):
+    return get_rooms(area_id, house_id)
 
-@app.get("/write/rooms", description="写入当前楼栋所有房间余额")
-async def write_room(area_id: str = Query(..., description="楼栋号id"), Cookie: Optional[str] = Header(None)):
-    houses = get_houses(area_id, Cookie)
-    for house in houses["houses"]:
-        threading.Thread(target=write_point, args=(json.dumps(get_rooms(area_id, house['id'], Cookie)),)).start()
-        time.sleep(1)
-    return {"code": 200, "message": "写入成功"}
+
+@app.get("/write/rooms", description="""写入当前楼栋所有房间余额
+    一般为宿舍楼 001001-001012""")
+async def write_room_data(area_id: str = Query(..., description="楼栋号id")):
+    return write_room(area_id)
